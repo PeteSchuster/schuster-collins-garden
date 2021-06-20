186c51a1b975e75b4cf4bddb7837331b9f225481
Pete Schuster <pas5027@gmail.com>
2021-06-20 19:37:15 -0400

    more

---
+++ src/pages/Year2021/data.ts
@@ -0,0 +1,7 @@
+type PlantNames = "agastache" | "amsonia hubrichtii" | "aster woods" | "calamagrostis" | "caryopteris" | "cherry laurels" | "coral bells" | "crape myrtle tuscarora" | "echinacea" | "echinacea" | "echinacea" | "fothergilla" | "hardy geraniums" | "itea" | "ivory halo dogwoods" | "lady fern" | "mexican feather grass" | "nepeta" | "rudbeckia" | "spiraea" | "spiraea" | "yarrow";
+
+type PlantRow = PlantNames[]; 
+
+export const PlantList: PlantRow[] = [
+  [],
+];
